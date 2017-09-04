2ea91aea36b93885c63830dc6d61d85a4a1a64d2
Marco Antonio Dominguez <madmzr@gmail.com>
2017-09-04 09:38:50 -0500

    Add configuration for Bloomer

---
+++ theme/reset.js
@@ -0,0 +1,53 @@
+const blocks = {
+  margin: 0,
+  padding: 0
+};
+
+const headings = {
+  'font-size': '100%',
+  'font-weight': 'normal'
+};
+
+const list = {
+  'list-style': 'none'
+}
+const form = {
+  margin: 0
+};
+
+const boxSizing = {
+  'box-sizing': 'border-box'
+}
+
+const media = {
+  'max-width': '100%'
+};
+
+const iframe = {
+  border: 0
+};
+
+const table = {
+  'border-collapse': 'collapse',
+  'border-spacing': 0
+};
+
+const tableElements = {
+  padding: 0,
+  'text-align': 'left'
+};
+
+const font = {
+  family: {
+    base: 'BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif'
+  }
+  size: {
+    base: 16
+  },
+  "line-height": {
+    base: 1.5
+  }
+}
+
+
+export {blocks, headings, list, form, boxSizing, media, iframe, table, tableElements};
---
+++ theme/settings.js
@@ -0,0 +1,11 @@
+const colors = {
+  'black': 'hsl(0, 0%, 4%)',
+  'black-bis': 'hsl(0, 0%, 7%)',
+  'black-ter': 'hsl(0, 0%, 14%)',
+  'grey-darker': 'hsl(0, 0%, 21%)',
+  'grey-dark': 'hsl(0, 0%, 29%)',
+  'grey': 'hsl(0, 0%, 48%)',
+  'grey-light': 'hsl(0, 0%, 71%)',
+  'grey-lighter': 'hsl(0, 0%, 86%)',
+  'white': 'hsl(0, 0%, 100%)'
+}
